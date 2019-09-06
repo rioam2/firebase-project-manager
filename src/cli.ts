31af0ce093c4d8475dd23ec27db53ab988e91f0e
Rio Martinez <rio@riomartinez.com>
2019-09-05 22:39:24 -0500

    implemented new CLI with inquirer and commander

--- src/cli.ts
+++ src/cli.ts
@@ -2,76 +2,13 @@
 import { Spinner } from 'cli-spinner';
 
 import { authenticate, deauthenticate } from './auth';
-import { createFirebaseProject, ERRORS, EVENTS } from './firebase';
-import { displayHelp } from './help';
+import { createFirebaseProject, EVENTS } from './firebase';
+import { CLI } from './util';
+
+const program = new CLI();
 
 Spinner.setDefaultSpinnerString(18);
 const spinner = new Spinner();
-
-export enum COMMANDS {
-	HELP = 'help',
-	LOGIN = 'login',
-	LOGOUT = 'logout',
-	PROJECT = 'project'
-}
-
-export enum PROJECT_OPERATIONS {
-	CREATE = 'create',
-	ADD = 'add'
-}
-
-export enum EXIT_CODE {
-	SUCCESS,
-	INVALID_COMMAND,
-	INVALID_ARGUMENTS,
-	UNHANDLED_ERROR
-}
-
-(async function main([command, ...args]: [COMMANDS, ...any[]]) {
-	try {
-		switch (command) {
-			case COMMANDS.LOGIN:
-				await authenticate(true);
-				break;
-			case COMMANDS.LOGOUT:
-				deauthenticate(true);
-				break;
-			case COMMANDS.PROJECT:
-				const [operation, ...rest] = args;
-				await handleProject(operation, ...rest);
-				break;
-			case COMMANDS.HELP:
-				const [cmd] = args;
-				displayHelp(cmd);
-				break;
-			default:
-				displayHelp(COMMANDS.HELP);
-				process.exit(EXIT_CODE.INVALID_COMMAND);
-		}
-	} catch (e) {
-		const handle = msg => {
-			spinner.stop(true);
-			console.error(`ERROR: ${msg}`);
-			if (msg === ERRORS.GCP_TERMS_OF_SERVICE) {
-				console.info(
-					'Please view the GCP console to review:',
-					'https://console.cloud.google.com'
-				);
-			} else if (msg === ERRORS.FIREBASE_TERMS_OF_SERVICE) {
-				console.info(
-					'Did you accept the Firebase terms of service? Visit this link and click, "Explore a demo project":',
-					'https://console.firebase.google.com'
-				);
-			}
-			process.exit(EXIT_CODE.UNHANDLED_ERROR);
-		};
-		if (e instanceof Error) {
-			handle(e.message);
-		} else {
-			handle(e);
-		}
-	}
-})(process.argv.slice(2) as [COMMANDS, ...any[]]);
 
 function handleProgress(event: EVENTS, data: any) {
 	if (!spinner.isSpinning()) {
@@ -96,26 +33,29 @@ function handleProgress(event: EVENTS, data: any) {
 	}
 }
 
-async function handleProject(operation: PROJECT_OPERATIONS, ...args: any[]) {
-	switch (operation) {
-		case PROJECT_OPERATIONS.CREATE:
-			const [name] = args;
-			await handleCreate(name);
-			break;
-		case PROJECT_OPERATIONS.ADD:
-			console.info('Not yet implemented.');
-		default:
-			displayHelp(COMMANDS.PROJECT);
-			process.exit(EXIT_CODE.INVALID_COMMAND);
-	}
-}
-
-async function handleCreate(name?: string) {
-	if (name) {
+program
+	.command('create <name=new-firebase-project>(New project name)')
+	.description('Create a new Firebase Project')
+	.action(async ({ name }) => {
 		const projectId = await createFirebaseProject(name, handleProgress);
 		console.log(`Successfully created new firebase project: ${projectId}`);
-	} else {
-		displayHelp(COMMANDS.PROJECT);
-		process.exit(EXIT_CODE.INVALID_ARGUMENTS);
-	}
-}
+	});
+
+program
+	.command('login')
+	.description('Login and save credentials')
+	.action(async () => {
+		await authenticate();
+		program.run();
+	});
+
+program
+	.command('logout')
+	.description('Logout and clear saved credentials')
+	.action(async () => {
+		await deauthenticate();
+		program.run();
+	});
+
+program.root().title('What would you like to do?');
+program.root().run();
--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,8 @@
+import chalk from 'chalk';
+import { Command } from 'commander';
+import { textSync as figlet } from 'figlet';
 import { cloudresourcemanager_v1, firebase_v1beta1 } from 'googleapis';
+import * as inquirer from 'inquirer';
 
 export async function waitOnOperation(
 	api: cloudresourcemanager_v1.Cloudresourcemanager | firebase_v1beta1.Firebase,
@@ -28,4 +32,188 @@ export function getRandomHexLen(length: number) {
 		rand += Math.floor(Math.random() * 0xf).toString(16);
 	}
 	return rand;
+}
+
+interface CommandArg {
+	name: string;
+	initialValue: string;
+	message: string;
+	required: boolean;
+}
+
+interface InquirerConfig {
+	[key: string]: Function | inquirer.ListQuestion<inquirer.Answers>;
+}
+
+export class CLI {
+	private args = process.argv;
+	private initialRun = true;
+	private prog = new Command();
+	private isInteractive = this.args.length < 3;
+	private currentCmdPathStr = '';
+	private currentCommand = this.prog;
+	private currentCommandArgs: CommandArg[] = [];
+	private inquirerConfig: InquirerConfig = {
+		'': {
+			choices: [{ name: 'Exit', value: 'exit' }],
+			type: 'list'
+		},
+		exit: () => {
+			process.exit();
+		}
+	};
+
+	public async run() {
+		if (this.isInteractive) {
+			this.formalizeCommandTree();
+			if (this.inquirerConfig) {
+				let configPath = '';
+				while (typeof this.inquirerConfig[configPath] !== 'function') {
+					console.clear();
+					const header = figlet('Firebase PM', 'Slant');
+					console.log(chalk.yellowBright(header));
+					const config = this.inquirerConfig[configPath];
+					configPath = ((await inquirer.prompt({
+						...config,
+						name: 'value'
+					})) as any).value;
+				}
+				await (this.inquirerConfig[configPath] as Function)();
+			}
+		} else {
+			if (this.initialRun) {
+				this.initialRun = false;
+				this.prog.parse(this.args);
+			}
+		}
+	}
+
+	public command(commandStr: string) {
+		this.parseCommandString(commandStr);
+		if (this.inquirerConfig[this.currentCmdPathStr] === undefined) {
+			const parentPath = this.getParentCmdPath();
+			const parent = this.inquirerConfig[parentPath];
+			if (typeof parent !== 'function') {
+				(parent.choices as any).push({
+					name: this.currentCmdPathStr,
+					value: this.currentCmdPathStr
+				});
+			}
+			this.inquirerConfig[this.currentCmdPathStr] = {
+				choices: [{ name: 'Back', value: parentPath }],
+				type: 'list'
+			};
+		}
+		return this;
+	}
+
+	public menu(menuName: string) {
+		return this.command(menuName);
+	}
+
+	public root() {
+		return this.command('');
+	}
+
+	public description(description: string) {
+		const parent = this.inquirerConfig[this.getParentCmdPath()];
+		if (typeof parent !== 'function') {
+			const choice = (parent.choices as any[]).find(
+				elt => elt.value === this.currentCmdPathStr
+			);
+			choice.name = description;
+		}
+		this.currentCommand.description(description);
+		return this;
+	}
+
+	public title(title: string) {
+		(this.inquirerConfig[this.currentCmdPathStr] as any).message = title;
+		return this;
+	}
+
+	public action(fn: (...args: any[]) => any) {
+		const cmdArgsCopy = this.currentCommandArgs.slice();
+
+		if (this.isInteractive) {
+			this.inquirerConfig[this.currentCmdPathStr] = async () => {
+				const argValues = {};
+				for (const arg of cmdArgsCopy) {
+					const config: inquirer.QuestionCollection = {
+						default: arg.initialValue,
+						message: arg.message,
+						name: arg.name,
+						validate(answer) {
+							return !arg.required || (arg.required && answer)
+								? true
+								: 'This is required';
+						}
+					};
+					argValues[arg.name] = (await inquirer.prompt(config))[arg.name];
+				}
+				return fn(argValues);
+			};
+		} else {
+			this.inquirerConfig[this.currentCmdPathStr] = fn;
+			this.currentCommand.action((...fnArgs) => {
+				const argMap = {};
+				cmdArgsCopy.forEach((arg, idx) => {
+					argMap[arg.name] = fnArgs[idx];
+				});
+				fn(argMap);
+			});
+		}
+		return this;
+	}
+
+	private parseCommandString(cmdStr: string) {
+		const cmdArgs: CommandArg[] = [];
+		const reqArgReg = /\<([^\>=]*)(?:=([^\>]*))?\>(?:\(([^)]*)\))?/g;
+		const optArgReg = /\[([^\]=]*)(?:=([^\]]*))?\](?:\(([^)]*)\))?/g;
+		cmdStr = cmdStr.replace(reqArgReg, (...matches: any[]) => {
+			const [name, initialValue, message] = matches.slice(1, -2);
+			cmdArgs.push({ name, initialValue, message, required: true });
+			return `<${name}>`;
+		});
+		cmdStr = cmdStr.replace(optArgReg, (...matches: any[]) => {
+			const [name, initialValue, message] = matches.slice(1, -2);
+			cmdArgs.push({ name, initialValue, message, required: false });
+			return `[${name}]`;
+		});
+
+		this.currentCommand = this.prog.command(cmdStr);
+		this.currentCommandArgs = cmdArgs;
+		this.currentCmdPathStr = cmdStr
+			.split('<')[0]
+			.split('[')[0]
+			.trim()
+			.replace(' ', '.');
+	}
+
+	private getParentCmdPath() {
+		const pathArr = this.currentCmdPathStr.split('.');
+		if (pathArr.length <= 1) {
+			return '';
+		} else {
+			pathArr.pop();
+			return pathArr.join('.');
+		}
+	}
+
+	private formalizeCommandTree() {
+		Object.entries(this.inquirerConfig).forEach(([key, value]) => {
+			if (typeof value === 'object') {
+				if ((value.choices as any).length === 0) {
+					this.inquirerConfig[key] = () => {};
+				} else {
+					const config = this.inquirerConfig[key] as any;
+					config.choices = config.choices.sort((a, b) => {
+						const aWeight = ['Exit', 'Back'].includes(a.name) ? -999 : 0;
+						const bWeight = ['Exit', 'Back'].includes(b.name) ? -999 : 0;
+						return bWeight - aWeight;
+					});
+				}
+			}
+		});
+	}
 }
