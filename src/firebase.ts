316199a9d0759f94ab1b68ac10711bf5a9ad0a8c
Rio Martinez <rio@riomartinez.com>
2019-09-12 19:11:00 -0500

    firebase api implementation

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,70 @@
+import { firebase_v1beta1 } from 'googleapis';
+
 import { GoogleApis } from './apis.types';
 import { ERRORS, EventCB, EVENTS } from './firebase.types';
 import { getRandomHexLen, noop, waitOnOperation } from './util';
+
+export async function getFirebaseWebappConfig(googleapis: GoogleApis, name: string);
+export async function getFirebaseWebappConfig(googleapis: GoogleApis, projectId: string, appId?: string);
+export async function getFirebaseWebappConfig(googleapis: GoogleApis, projectId: string, appId?: string) {
+	let name = projectId;
+	if (projectId.includes('/') || appId) {
+		name = `projects/${projectId}/webApps/${appId}`;
+	}
+	const { data } = await googleapis.firebase.projects.webApps.getConfig({ name });
+	return data;
+}
+
+export async function getFirebaseWebapp(googleapis: GoogleApis, name: string);
+export async function getFirebaseWebapp(googleapis: GoogleApis, projectId: string, appId?: string);
+export async function getFirebaseWebapp(googleapis: GoogleApis, projectId: string, appId?: string) {
+	let name = projectId;
+	if (projectId.includes('/') || appId) {
+		name = `projects/${projectId}/webApps/${appId}`;
+	}
+	const { data } = await googleapis.firebase.projects.webApps.get({ name });
+	return data;
+}
+
+export async function createFirebaseWebapp(
+	googleapis: GoogleApis,
+	projectId: string,
+	displayName?: string,
+	appUrls?: string[]
+) {
+	const parent = `projects/${projectId}`;
+	const appRequest = { parent, requestBody: { displayName, appUrls } };
+	const operation = await googleapis.firebase.projects.webApps.create(appRequest);
+	const data = await waitOnOperation(googleapis.firebase, operation);
+	return data.response as firebase_v1beta1.Schema$WebApp;
+}
+
+export async function listFirebaseProjectApps(
+	googleapis: GoogleApis,
+	projectId: string,
+	pageSize?: number,
+	pageToken?: string
+) {
+	const parent = `projects/${projectId}`;
+	const { data } = await googleapis.firebase.projects.searchApps({ pageSize, pageToken, parent });
+	return data.apps;
+}
+
+export async function getFirebaseProject(googleapis: GoogleApis, projectId: string) {
+	const name = `projects/${projectId}`;
+	const { data } = await googleapis.firebase.projects.get({ name });
+	return data;
+}
+
+export async function listAvailableProjects(googleapis: GoogleApis, pageSize?: number, pageToken?: string) {
+	const { data } = await googleapis.firebase.availableProjects.list({ pageSize, pageToken });
+	return data.projectInfo;
+}
+
+export async function listFirebaseProjects(googleapis: GoogleApis, pageSize?: number, pageToken?: string) {
+	const { data } = await googleapis.firebase.projects.list({ pageSize, pageToken });
+	return data.results || [];
+}
 
 export async function createFirebaseProject(googleapis: GoogleApis, name: string, cb: EventCB = noop) {
 	const projectId = await createGCProject(googleapis, name, cb);
@@ -11,9 +75,8 @@ export async function createFirebaseProject(googleapis: GoogleApis, name: string
 export async function addFirebaseFeatures(googleapis: GoogleApis, projectId: string, cb: EventCB = noop) {
 	const project = `projects/${projectId}`;
 	cb(EVENTS.ADD_FIREBASE_FEATURES_STARTED);
-	const response = await googleapis.firebase.projects.addFirebase({ project });
-	const operationName = response.data.name as string;
-	await waitOnOperation(googleapis.firebase, operationName);
+	const operation = await googleapis.firebase.projects.addFirebase({ project });
+	await waitOnOperation(googleapis.firebase, operation);
 	cb(EVENTS.ADD_FIREBASE_FEATURES_SUCCEEDED);
 }
 
@@ -23,10 +86,9 @@ export async function createGCProject(googleapis: GoogleApis, name: string, cb: 
 		cb(EVENTS.PROJECT_CREATION_ATTEMPT_STARTED, projectId);
 		try {
 			const request = { requestBody: { projectId, name } };
-			const response = await googleapis.cloudresourcemanager.projects.create(request);
-			const operationName = response.data.name as string;
+			const operation = await googleapis.cloudresourcemanager.projects.create(request);
 			cb(EVENTS.PROJECT_CREATION_STARTED, projectId);
-			const data = await waitOnOperation(googleapis.cloudresourcemanager, operationName);
+			const data = await waitOnOperation(googleapis.cloudresourcemanager, operation);
 			cb(EVENTS.PROJECT_CREATION_SUCCEEDED, projectId);
 			return (data.response as any).projectId as string;
 		} catch (e) {
--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,32 @@ import { ShiftArgs } from './util';
 export { ERRORS, EVENTS } from './firebase.types';
 export { oauth2Client, authenticate, deauthenticate } from './auth';
 
-export const addFirebaseFeatures = (...args: ShiftArgs<typeof firebase.addFirebaseFeatures>) =>
-	firebase.addFirebaseFeatures(googleapis, ...args);
-
 export const createFirebaseProject = (...args: ShiftArgs<typeof firebase.createFirebaseProject>) =>
 	firebase.createFirebaseProject(googleapis, ...args);
 
 export const createGCProject = (...args: ShiftArgs<typeof firebase.createGCProject>) =>
 	firebase.createGCProject(googleapis, ...args);
+
+export const addFirebaseFeatures = (...args: ShiftArgs<typeof firebase.addFirebaseFeatures>) =>
+	firebase.addFirebaseFeatures(googleapis, ...args);
+
+export const listFirebaseProjects = (...args: ShiftArgs<typeof firebase.listFirebaseProjects>) =>
+	firebase.listFirebaseProjects(googleapis, ...args);
+
+export const listAvailableProjects = (...args: ShiftArgs<typeof firebase.listAvailableProjects>) =>
+	firebase.listAvailableProjects(googleapis, ...args);
+
+export const getFirebaseProject = (...args: ShiftArgs<typeof firebase.getFirebaseProject>) =>
+	firebase.getFirebaseProject(googleapis, ...args);
+
+export const listFirebaseProjectApps = (...args: ShiftArgs<typeof firebase.listFirebaseProjectApps>) =>
+	firebase.listFirebaseProjectApps(googleapis, ...args);
+
+export const createFirebaseWebapp = (...args: ShiftArgs<typeof firebase.createFirebaseWebapp>) =>
+	firebase.createFirebaseWebapp(googleapis, ...args);
+
+export const getFirebaseWebapp = (...args: ShiftArgs<typeof firebase.getFirebaseWebapp>) =>
+	firebase.getFirebaseWebapp(googleapis, ...args);
+
+export const getFirebaseWebappConfig = (...args: ShiftArgs<typeof firebase.getFirebaseWebappConfig>) =>
+	firebase.getFirebaseWebappConfig(googleapis, ...args);
