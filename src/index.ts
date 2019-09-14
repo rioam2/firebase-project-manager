e15fe289d3d287216f6d0ada6c20ebf0e6f20fe6
Rio Martinez <35539750+rioam2@users.noreply.github.com>
2019-09-13 19:10:16 -0500

    add documentation (#19)
    * added function headers
    
    * added readme

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,146 @@
 import { googleapis } from './apis';
+import * as auth from './auth';
 import * as firebase from './firebase';
-import { ShiftArgs } from './util';
+import { EventCB } from './firebase.types';
+import { noop } from './util';
 
-export { ERRORS, EVENTS } from './firebase.types';
-export { oauth2Client, authenticate, deauthenticate } from './auth';
+export { ERRORS, EVENTS, EventCB } from './firebase.types';
 
-export const createFirebaseProject = (...args: ShiftArgs<typeof firebase.createFirebaseProject>) =>
-	firebase.createFirebaseProject(googleapis, ...args);
+/** Static OAuth client managed by `authenticate` and `deauthenticate` */
+export const oauth2Client = auth.oauth2Client;
 
-export const createGCProject = (...args: ShiftArgs<typeof firebase.createGCProject>) =>
-	firebase.createGCProject(googleapis, ...args);
+/**
+ * Authenticates client with Firebase Project Manager and retains a refresh token in the
+ * system's configuration store for later usage.
+ * @param verbose If true, authentication status will be logged with `console.log`.
+ * @return Void promise resolved upon completion
+ */
+export const authenticate = auth.authenticate;
 
-export const addFirebaseFeatures = (...args: ShiftArgs<typeof firebase.addFirebaseFeatures>) =>
-	firebase.addFirebaseFeatures(googleapis, ...args);
+/**
+ * Deauthenticates the currently logged in user, revokes and deletes the currently cached
+ * refresh_token stored in the system's configuration store.
+ * @param verbose If true, authentication status will be logged with `console.log`.
+ * @return Void promise resolved upon completion
+ */
+export const deauthenticate = auth.deauthenticate;
 
-export const listFirebaseProjects = (...args: ShiftArgs<typeof firebase.listFirebaseProjects>) =>
-	firebase.listFirebaseProjects(googleapis, ...args);
+/**
+ * Creates a new Firebase Project via a two step process of first creating a CGP Project
+ * and then secondly adding Firebase resources to it.
+ * @param name Desired display name for the underlying GCP project. If available, this name
+ * will also be used as a `projectId` by replacing all non-whitespace characters with '-'
+ * and making all alphabetic characters lower-case.
+ * @param cb function called with progress of project creation.
+ * @return Promise to the newly created project's `projectId`.
+ */
+export function createFirebaseProject(name: string, cb: EventCB = noop) {
+	return firebase.createFirebaseProject(googleapis, name, cb);
+}
 
-export const listAvailableProjects = (...args: ShiftArgs<typeof firebase.listAvailableProjects>) =>
-	firebase.listAvailableProjects(googleapis, ...args);
+/**
+ * Creates a new GCP project using Google `cloudresourcemanager`.
+ * @param name Desired display name for the underlying GCP project. If available, this name
+ * will also be used as a `projectId` by replacing all non-whitespace characters with `-`
+ * and making all alphabetic characters lower-case.
+ * @param cb function called with progress of project creation.
+ * @return Promise to the newly created project's `projectId`.
+ */
+export function createGCProject(name: string, cb: EventCB = noop) {
+	return firebase.createGCProject(googleapis, name, cb);
+}
 
-export const getFirebaseProject = (...args: ShiftArgs<typeof firebase.getFirebaseProject>) =>
-	firebase.getFirebaseProject(googleapis, ...args);
+/**
+ * Adds Firebase resources to a GCP Project
+ * @param projectId projectId of the GCP Project to add Firebase features to.
+ * @param cb function called with progress of project creation.
+ * @return Void promise resolved upon completion
+ */
+export function addFirebaseFeatures(projectId: string, cb: EventCB = noop) {
+	return firebase.addFirebaseFeatures(googleapis, projectId, cb);
+}
 
-export const listFirebaseProjectApps = (...args: ShiftArgs<typeof firebase.listFirebaseProjectApps>) =>
-	firebase.listFirebaseProjectApps(googleapis, ...args);
+/**
+ * Lists all GCP Projects with Firebase resources.
+ * @param pageSize The maximum number of Projects to return
+ * @param pageToken Token returned from a previous call indicating where in the
+ * set of Projects to resume listing.
+ * @return Promise to an array of Firebase Projects
+ */
+export function listFirebaseProjects(pageSize?: number, pageToken?: string) {
+	return firebase.listFirebaseProjects(googleapis, pageSize, pageToken);
+}
+/**
+ * Lists all GCP Projects lacking Firebase resources, but that are available to become Firebase
+ * projects.
+ * @param pageSize The maximum number of Projects to return
+ * @param pageToken Token returned from a previous call indicating where in the
+ * set of Projects to resume listing.
+ * @return Promise to an array of GCP Projects
+ */
+export function listAvailableProjects(pageSize?: number, pageToken?: string) {
+	return firebase.listAvailableProjects(googleapis, pageSize, pageToken);
+}
 
-export const createFirebaseWebapp = (...args: ShiftArgs<typeof firebase.createFirebaseWebapp>) =>
-	firebase.createFirebaseWebapp(googleapis, ...args);
+/**
+ * Retrieves information for a given Firebase project.
+ * @param projectId projectId of the Firebase project to retrieve
+ * @return Promise to a Firebase Project
+ */
+export function getFirebaseProject(projectId: string) {
+	return firebase.getFirebaseProject(googleapis, projectId);
+}
 
-export const getFirebaseWebapp = (...args: ShiftArgs<typeof firebase.getFirebaseWebapp>) =>
-	firebase.getFirebaseWebapp(googleapis, ...args);
+/**
+ * Lists all applications associated with a Firebase Project.
+ * @param projectId projectId of the Firebase project to retrieve apps of
+ * @param pageSize The maximum number of Apps to return
+ * @param pageToken Token returned from a previous call indicating where in the
+ * set of Apps to resume listing.
+ */
+export function listFirebaseProjectApps(projectId: string, pageSize?: number, pageToken?: string) {
+	return firebase.listFirebaseProjectApps(googleapis, projectId, pageSize, pageToken);
+}
 
-export const getFirebaseWebappConfig = (...args: ShiftArgs<typeof firebase.getFirebaseWebappConfig>) =>
-	firebase.getFirebaseWebappConfig(googleapis, ...args);
+/**
+ * Creates a new web application under a given Firebase Project.
+ * @param projectId projectId of the parent Firebase project to create an application for
+ * @param displayName User-assigned display name of the App.
+ * @param appUrls Fully qualified URLs where the App is hosted.
+ */
+export function createFirebaseWebapp(projectId: string, displayName?: string, appUrls?: string[]) {
+	return firebase.createFirebaseWebapp(googleapis, projectId, displayName, appUrls);
+}
+
+/**
+ * Retrieves information for a given Firebase Project's app.
+ * @param name Fully qualified identifier for the webapp (Eg: `/projects/.../webApp/...`)
+ */
+export function getFirebaseWebapp(name: string): ReturnType<typeof firebase.getFirebaseWebapp>;
+/**
+ * Retrieves information for a given Firebase Project's app.
+ * @param projectId projectId of the parent Firebase project
+ * @param appId The webapp's ID.
+ */
+export function getFirebaseWebapp(projectId: string, appId?: string): ReturnType<typeof firebase.getFirebaseWebapp>;
+export function getFirebaseWebapp(projectId: string, appId?: string) {
+	return firebase.getFirebaseWebapp(googleapis, projectId, appId);
+}
+
+/**
+ * Retrieves configuration information for a given Firebase Project's app.
+ * @param name Fully qualified identifier for the webapp (Eg: `/projects/.../webApp/...`)
+ */
+export function getFirebaseWebappConfig(name: string): ReturnType<typeof firebase.getFirebaseWebappConfig>;
+/**
+ * Retrieves configuration information for a given Firebase Project's app.
+ * @param projectId projectId of the parent Firebase project
+ * @param appId The webapp's ID.
+ */
+export function getFirebaseWebappConfig(
+	projectId: string,
+	appId?: string
+): ReturnType<typeof firebase.getFirebaseWebappConfig>;
+export function getFirebaseWebappConfig(projectId: string, appId?: string) {
+	return firebase.getFirebaseWebappConfig(googleapis, projectId, appId);
+}
